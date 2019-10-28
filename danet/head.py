292334bcc7ca43fecf402cb453f9711d19aa44e2
leftthomas <leftthomas@qq.com>
2019-10-28 23:22:12 +0800

    finish DANet

--- danet/head.py
+++ danet/head.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import fvcore.nn.weight_init as weight_init
 import numpy as np
+import torch
 from detectron2.layers import Conv2d, ShapeSpec
 from detectron2.modeling.meta_arch.semantic_seg import SEM_SEG_HEADS_REGISTRY
 from torch import nn
@@ -22,10 +23,9 @@ class DANetHead(nn.Module):
         conv_dims = cfg.MODEL.SEM_SEG_HEAD.CONVS_DIM
         self.common_stride = cfg.MODEL.SEM_SEG_HEAD.COMMON_STRIDE
         norm = cfg.MODEL.SEM_SEG_HEAD.NORM
-        self.loss_weight = cfg.MODEL.SEM_SEG_HEAD.LOSS_WEIGHT
         # fmt: on
 
-        self.scale_heads = []
+        self.scale_pam_heads = []
         for in_feature in self.in_features:
             head_ops = []
             head_length = max(
@@ -49,18 +49,65 @@ class DANetHead(nn.Module):
                     head_ops.append(
                         nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
                     )
-            self.scale_heads.append(nn.Sequential(*head_ops))
-            self.add_module(in_feature, self.scale_heads[-1])
+            self.scale_pam_heads.append(nn.Sequential(*head_ops))
+            self.add_module(in_feature + '_pam', self.scale_pam_heads[-1])
+
+        self.scale_cam_heads = []
+        for in_feature in self.in_features:
+            head_ops = []
+            head_length = max(
+                1, int(np.log2(feature_strides[in_feature]) - np.log2(self.common_stride))
+            )
+            for k in range(head_length):
+                norm_module = nn.GroupNorm(32, conv_dims) if norm == "GN" else None
+                conv = Conv2d(
+                    feature_channels[in_feature] if k == 0 else conv_dims,
+                    conv_dims,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=not norm,
+                    norm=norm_module,
+                    activation=F.relu,
+                )
+                weight_init.c2_msra_fill(conv)
+                head_ops.append(conv)
+                if feature_strides[in_feature] != self.common_stride:
+                    head_ops.append(
+                        nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+                    )
+            self.scale_cam_heads.append(nn.Sequential(*head_ops))
+            self.add_module(in_feature + '_cam', self.scale_cam_heads[-1])
+
         self.predictor = Conv2d(conv_dims, num_classes, kernel_size=1, stride=1, padding=0)
         weight_init.c2_msra_fill(self.predictor)
 
     def forward(self, features, targets=None):
         for i, f in enumerate(self.in_features):
             if i == 0:
-                x = self.scale_heads[i](features[f])
+                pam = self.scale_pam_heads[i](features[f])
+                cam = self.scale_cam_heads[i](features[f])
             else:
-                x = x + self.scale_heads[i](features[f])
-        x = self.predictor(x)
+                pam = pam + self.scale_pam_heads[i](features[f])
+                cam = cam + self.scale_cam_heads[i](features[f])
+
+        b, c, h, w = pam.size()
+        B_T = pam.view(b, c, -1)
+        B = B_T.transpose(-1, -2).contiguous()
+        pam_weight = F.softmax(torch.matmul(B, B_T), dim=-1).view(b, 1, h * w, h * w)
+        weighted_pam = torch.matmul(pam_weight, pam.view(b, c, h * w, 1)).view(b, c, h, w)
+        sum_pam = pam + weighted_pam
+
+        b, c, h, w = cam.size()
+        A = cam.view(b, c, -1)
+        A_T = A.transpose(-1, -2).contiguous()
+        cam_weight = F.softmax(torch.matmul(A, A_T), dim=-1).view(b, 1, c, c)
+        weighted_cam = torch.matmul(cam_weight, cam.view(b, c, h * w).transpose(-1, -2).contiguous()
+                                    .view(b, h * w, c, 1)).view(b, h * w, c).transpose(-1, -2).contiguous() \
+            .view(b, c, h, w)
+        sum_cam = cam + weighted_cam
+
+        x = self.predictor(sum_pam + sum_cam)
         x = F.interpolate(x, scale_factor=self.common_stride, mode="bilinear", align_corners=False)
 
         if self.training:
